fc4063908893c2aca2180788ed31a43a5b697eef
maxnth <maximilian.noeth@protonmail.com>
2021-04-28 11:35:38 +0200

    temporarily remove import

--- pagetools/cli/__init__.py
+++ pagetools/cli/__init__.py
@@ -4,7 +4,6 @@ from pagetools.cli.analytics.get_codec import get_codec_cli
 
 from pagetools.cli.transformations.extract import extract_cli
 from pagetools.cli.transformations.regularize import regularize_cli
-from pagetools.cli.transformations.modify_text_indices import change_text_index
 
 
 @click.group()
@@ -19,4 +18,3 @@ cli.add_command(get_codec_cli)
 
 cli.add_command(extract_cli)
 cli.add_command(regularize_cli)
-cli.add_command(change_text_index)
