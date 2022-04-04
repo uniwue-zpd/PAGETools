bbf9a0e94bfd9d0e5e01d5931311b34472649ee6
maxnth <maximilian.noeth@protonmail.com>
2022-04-04 13:44:25 +0200

    split CLI and line2page logic

---
+++ pagetools/cli/transformations/line2page.py
@@ -0,0 +1,32 @@
+from pagetools.src.line2page.line2page import Line2Page
+
+from pathlib import Path
+
+import click
+
+
+@click.command("line2page", help="Merges line images and text to combined image with PAGE XML annotation")
+@click.option('-c', '--creator', default='user', help='Creator tag for PAGE XML')
+@click.option('-s', '--source-folder', default=Path.cwd(), help='Path to images and GT')
+@click.option('-i', '--image-folder', default='', help='Path to images')
+@click.option('-gt', '--gt-folder', default='', help='Path to GT')
+@click.option('-d', '--dest-folder', default=Path(Path.cwd(), 'merged'), help='Path to merge objects')
+@click.option('-e', '--ext', default='.bin.png', help='Image extension')
+@click.option('-p', '--pred', default=False, type=bool, help='Set flag to also store .pred.txt')
+@click.option('-l', '--lines', default=20, type=int, help='Lines per page')
+@click.option('-ls', '--line-spacing', default=5, type=int, help='Line spacing (in pixel)')
+@click.option('-b', '--border', default=10, type=int, help='Border (in pixel)')
+@click.option('--debug', default=False, help='Prints debug XML')
+@click.option('--threads', default=16, type=int, help='Thread count to be used')
+def cli(creator, source_folder, image_folder, gt_folder, dest_folder, ext, pred, lines, line_spacing, border, debug,
+        threads):
+    image_path = source_folder if not image_folder else image_folder
+    gt_path = source_folder if not gt_folder else gt_folder
+
+    option_object = Line2Page(creator, source_folder, image_path, gt_path, dest_folder, ext, pred, lines, line_spacing,
+                              border, debug, threads)
+    option_object.match_files()
+
+
+if __name__ == '__main__':
+    cli()
--- pagetools/src/line2page/line2page.py
+++ pagetools/src/line2page/line2page.py
@@ -2,14 +2,12 @@
 import pathlib
 import stat
 
-import click
 import lxml
 
 #  remove this
 import argparse
 
 # keep this
-#import os.path
 import time
 import glob
 from pathlib import Path
@@ -42,7 +40,7 @@ def get_text(file):
         return data
 
 
-class CliOptions:
+class Line2Page:
     """Object, which includes the meta data
     source, image_folder, gt_folder, dest_folder are Path objects
     """
@@ -93,72 +91,6 @@ class CliOptions:
                         print("WARNING: The File " + str(self.gt_folder) + img_name + ".pred.txt could not be found! Omitting line from page")
                 self.matches.append(self.pairing.copy())
             else:
-                print(
-                    "WARNING: The File " + str(self.gt_folder) + img_name + ".gt.txt could not be found! Omitting line from page")
+                print("WARNING: The File " + str(self.gt_folder) + img_name + ".gt.txt could not be found! Omitting line from page")
         print("matching files")
 
-
-@click.command()
-@click.option('-c', '--creator', default='user', help='Creator tag for PAGE XML')
-@click.option('-s', '--source_folder', default=Path.cwd(), help='Path to images and GT')
-@click.option('-i', '--image_folder', default='', help='Path to images')
-@click.option('-gt', '--gt_folder', default='', help='Path to GT')
-@click.option('-d', '--dest_folder', default=Path(Path.cwd(), 'merged'), help='Path to merge objects')
-@click.option('-e', '--ext', default='.bin.png', help='image extension')
-@click.option('-p', '--pred', default=False, type=bool, help='Set Flag to also store .pred.txt')
-@click.option('-l', '--lines', default=20, type=int, help='lines per page')
-@click.option('-ls', '--line_spacing', default=5, type=int, help='line spacing')
-@click.option('-b', '--border', default=10, type=int, help='border in px')
-@click.option('--debug', default=False, help='prints debug xml')
-@click.option('--threads', default=16, type=int, help='thread count to be used')
-def cli(creator, source_folder, image_folder, gt_folder, dest_folder, ext, pred, lines, line_spacing, border, debug,
-        threads):
-    """Initialises a CLIArgument object, which saves the added cli options."""
-    # replace argparse with click
-    # self.parser = argparse.ArgumentParser(description='python script to merge GT lines to page images and xml')
-    click.echo("start cli()")
-    if image_folder == "":
-        image_path = source_folder
-    else:
-        image_path = image_folder
-    if gt_folder == "":
-        gt_path = source_folder
-    else:
-        gt_path = gt_folder
-    option_object = CliOptions(creator, source_folder, image_path, gt_path, dest_folder, ext, pred, lines, line_spacing,
-                               border, debug, threads)
-    click.echo("object created")
-    option_object.match_files()
-    click.echo("cli() finished")
-
-
-if __name__ == '__main__':
-    """python script to merge GT lines to page images and xml
-
-        tic = time counter
-        options = object with the desired meta data
-    """
-    tic = time.perf_counter()
-    # options = cli(standalone_mode=False)
-    # options.match_files()
-    click.echo("call cli()")
-    cli()
-    click.echo("\nProgramm finished")
-
-# help(Parser)
-# help(main)
-
-path = "/home/User/Documents/file.txt"
-obj = Path(path)
-print(obj)
-print(obj.name)
-print(obj.is_dir())
-cur_dir = Path.cwd()
-print(cur_dir)
-print(cur_dir.is_dir())
-# print(os.path.basename(path))
-name = strip_path(path.split('.')[0])
-name2 = strip_path(path)
-print("\n" + path)
-print(name)
-print(name2)
